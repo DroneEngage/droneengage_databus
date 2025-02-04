a91d130abc5bf3e171f15e934f1d713f01aacc8a
HefnySco <mohammad.hefny@gmail.com>
2025-02-05 00:58:52 +0300

    update: nodejs

--- nodejs/client.js
+++ nodejs/client.js
@@ -1,3 +1,4 @@
+const readline = require('readline');
 const dgram = require('dgram');
 const CModule = require('./de_module'); // Adjust the path as necessary
 const {CMyFacade} = require('./de_facade_base'); // Adjust path as necessary
@@ -8,6 +9,19 @@ const DEFAULT_UDP_DATABUS_PACKET_SIZE = 8192;
 
 const cModule = new CModule();
 const baseFacade = new CMyFacade(cModule);
+
+// Function to wait for a key press
+function waitForKeyPress(callback) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    rl.on('line', () => {
+        rl.close(); // Close the readline interface
+        callback(); // Call the provided callback function
+    });
+}
 
 function generateRandomModuleId() {
     return Array.from({ length: 12 }, () => Math.floor(Math.random() * 10)).join('');
@@ -28,13 +42,15 @@ if (require.main === module) {
     console.log(SUCCESS_CONSOLE_BOLD_TEXT + "./client sample_mod_js 60000 61111" + NORMAL_CONSOLE_TEXT);
     console.log(INFO_CONSOLE_BOLD_TEXT + "If the drone engage is running on port 60000 it will connect to it and appear in the WebClient Details tab as a module named sample_mod_js." + NORMAL_CONSOLE_TEXT);
     console.log("Press any key to continue ...");
-    // Uncomment below for waiting for input
-    // process.stdin.once('data', () => {});
-
+    
+    
     const args = process.argv.slice(2);
     if (args.length < 3) {
-        console.log(INFO_CONSOLE_BOLD_TEXT + "Insufficient arguments. Usage: app module_name broker_port(60000) listen_port(60003)" + NORMAL_CONSOLE_TEXT);
-        process.exit(1);
+        waitForKeyPress(() => {
+            console.log(INFO_CONSOLE_BOLD_TEXT + "Insufficient arguments. Usage: app module_name broker_port(60000) listen_port(60003)  e.g.: node client.js nodejs 60000 61234" + NORMAL_CONSOLE_TEXT);
+            process.exit(1);
+        });
+        return; // Exit the current execution context
     }
 
     const moduleName = args[0];
